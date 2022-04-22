02a8b017a3476e9250fcb564f98a12847257c01e
Muhammadfaizanarain <102913256+Muhammadfaizanarain@users.noreply.github.com>
2022-04-22 01:12:44 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+
+
+
+num1 = 1;
+num2 = 9/5;
+num3 = 32;
+
+function sum(){
+  num1 = parseInt(document.querySelector("#num1").value);
+  result = document.querySelector("#result");
+   myvalue = num1*9/5+32;
+   result.value = myvalue;
+   
+}
+
